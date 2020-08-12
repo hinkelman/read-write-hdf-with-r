c47860bfeafd068cda07956429ac2a3701ec02c1
Travis Hinkelman <travis.hinkelman@gmail.com>
2020-08-11 20:20:14 -0700

    Editing initial few slides

--- read-write-hdf-with-r.R
+++ read-write-hdf-with-r.R
@@ -1,7 +1,7 @@
 library(rhdf5)
 
 path = "sjr1500_omr5000.h5"
-fc <- h5ls(path)
-unique(fc$group)
+file_contents <- h5ls(path)
+unique(file_contents$group)
 h5readAttributes(path, "hydro")
 h5readAttributes(path, "hydro/data/channel area")
